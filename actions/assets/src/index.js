78d7288fe16120eab33b2ceb06429af7f0992624
Pavel Anikin <snwteam5@gmail.com>
2025-03-06 13:04:03 +0400

    feat: add initial node modules and configuration files for project dependencies

---
+++ actions/assets/src/index.js
@@ -0,0 +1,90 @@
+const fs = require("fs");
+const core = require("@actions/core");
+const { execSync } = require('child_process');
+const path = require("path");
+const Ajv = require('ajv');
+
+
+
+
+
+async function run() {
+    try {
+
+        jsonFile = core.getInput('asset-config-path');
+        const configPath = path.resolve(jsonFile);
+        console.log(`Reading asset config from ${configPath}`)
+
+        if (!fs.existsSync(jsonFile)) {
+            core.setFailed(`File not found: ${jsonFile}`);
+            return;
+        }
+
+        const fileContent = fs.readFileSync(jsonFile, 'utf8');
+
+        let config;
+        try {
+            config = JSON.parse(fileContent);
+        }
+        catch (error) {
+            core.setFailed(`Error parsing JSON file: ${error.message}`);
+            return;
+        }
+
+        const schemaPath = path.resolve(__dirname, 'config.schema.json');
+        if (!fs.existsSync(schemaPath)) {
+            core.setFailed(`Schema file not found: ${schemaPath}`);
+            return;
+        }
+
+        const schemaContent = fs.readFileSync(schemaPath, 'utf8');
+
+        let schema;
+        try {
+            schema = JSON.parse(schemaContent);
+        }
+        catch (error) {
+            core.setFailed(`Error parsing JSON schema: ${error.message}`);
+            return;
+        }
+
+        const ajv = new Ajv();
+        const validate = ajv.compile(schema);
+        const valid = validate(json);
+        if (!valid) {
+            constErrors = ajv.errorsText(validate.errors);
+            return;
+        }
+
+        let createArchives = [];
+
+        for (const archiveItem of config.archives) {
+            let folder = archiveItem.folder;
+            let archive = archiveItem.name;
+
+            if (!fs.existsSync(folder)) {
+                throw new Error(`Folder not found: ${folder}`);
+            }
+
+            const outputFile = `${archive}.tgz`;
+            const command = `tar -czf ${outputFile} dist/${folder}`;
+
+            execSync(command, {
+                cwd: process.env.GITHUB_WORKSPACE,
+                stdio: "inherit",
+            });
+
+            createArchives.push(outputFile)
+            core.info(`Creating archive ${outputFile} from ${folder}`);
+        }
+
+        core.setOutput('archives', createArchives);
+        core.info(`Output archives: ${createArchives}`);
+
+    }
+    catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+run();
