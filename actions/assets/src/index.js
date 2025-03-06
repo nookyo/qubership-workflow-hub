5f971d53fcda4d1f763e1fbf4c57c87387b9896c
Pavel Anikin <snwteam5@gmail.com>
2025-03-06 15:56:15 +0400

    fix: adjust success message formatting for improved readability in assets upload

--- actions/assets/src/index.js
+++ actions/assets/src/index.js
@@ -17,7 +17,7 @@ async function assetsUpload(dist_path, ref) {
                 console.log(`🔄 Uploading ${fullPath} to ${dist_path}/${ref}/${file}`);
             }
         }
-        core.info(`-----------------------------------------------\n`)
+        core.info(`\n-----------------------------------------------`)
     } catch (err) {
         core.setFailed(`❗️ Unable to scan directory: ${err}`);
         throw err;
@@ -79,7 +79,7 @@ async function run() {
             core.setFailed(`❗️ Configuration file is invalid: ${constErrors}`);
             return;
         }
-        core.warning(`Config file is valid: ${valid}`);
+        core.warning(`Config file is valid: ${valid}\n`);
 
         // Create dist folder for storing archives
         fs.mkdirSync(dist_path, { recursive: true })
@@ -117,7 +117,7 @@ async function run() {
             core.info(`🧱 Creating archive ${outputFile} from ${folder} archiveType: ${archiveType}`);;
         }
 
-        core.info(`-----------------------------------------------\n`)
+        core.info(`\n-----------------------------------------------`)
         if (upload === 'true') {
             await assetsUpload(dist_path, ref);
         }
