e68ba3d182dc1d18d657192bcab438c8f058e56c
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 12:28:28 +0400

    fix: Update package retrieval to fetch all versions using the new API endpoint

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -2,30 +2,23 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const repository = github.context.repo;
-    console.log(`Assembly run on: ${repository.owner}/${repository.repo}`);
-    console.log(`Event: ${github.context.eventName}`);
+  const token = process.env.GITHUB_TOKEN;
+  const octokit = github.getOctokit(token);
 
-    const token = process.env.GITHUB_TOKEN;
-    const [owner] = process.env.GITHUB_REPOSITORY.split('/');
-    const octokit = github.getOctokit(token);
-
-    try {
-
-        const response = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}', {
-            package_type: 'container',
-            package_name: 'qubership-dbaas',
-            username: 'nookyo',
-            headers: {
-              'X-GitHub-Api-Version': '2022-11-28'
-            }
-          });
-
-        console.log("Packages:", response.data);
-    } catch (error) {
-        console.error("Error:", error.message);
-        console.error("Details:", error.response?.data || error);
-    }
+  try {
+    const response = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
+      username: 'nookyo',            // замените на нужное имя пользователя
+      package_type: 'container',      // укажите тип пакета (например, 'container', 'npm', 'maven', 'nuget', и т.д.)
+      package_name: 'qubership-dbaas',// имя пакета
+      headers: {
+         'X-GitHub-Api-Version': '2022-11-28'
+      }
+    });
+    console.log("Все версии пакета:", response.data);
+  } catch (error) {
+    console.error("Ошибка:", error.message);
+    console.error("Детали:", error.response?.data || error);
+  }
 }
 
 run();
