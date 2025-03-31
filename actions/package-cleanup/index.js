0a5c2d6126953832a878b7724ec874efba68e5e3
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 15:03:31 +0400

    fix: Remove package type filter in package cleanup action

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -12,7 +12,7 @@ async function run() {
 
          const response = await octokit.request('GET /users/{username}/packages', {
             username: 'nookyo',
-            package_type: 'container',
+            package_type: '',
             headers: {
               'X-GitHub-Api-Version': '2022-11-28'
             }
