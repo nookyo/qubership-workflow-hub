f5dda32be6d550a4fddb8e3caa6c720e7ba57d88
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 11:41:12 +0400

    fix: Improve logging messages and refactor GraphQL query error handling in package cleanup action

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -2,13 +2,10 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { graphql } = require('@octokit/graphql');
 
-
-
 async function run() {
-
     const repository = github.context.repo;
-    console.log(`Assembly ru on: ${repository.owner}/${repository.repo}`);
-    console.log(`event: ${github.context.eventName}`);
+    console.log(`Assembly run on: ${repository.owner}/${repository.repo}`);
+    console.log(`Event: ${github.context.eventName}`);
 
     const token = process.env.GITHUB_TOKEN;
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
@@ -16,7 +13,7 @@ async function run() {
 
     const query = `
     query($owner: String!, $name: String!) {
-      repository(owner: ${owner}, name: ${repo}) {
+      repository(owner: $owner, name: $name) {
         packages(first: 100) {
           nodes {
             name
@@ -30,25 +27,24 @@ async function run() {
     }
   `;
 
-    graphql(query, {
-        owner,
-        name: repo,
-        headers: {
-            authorization: `token ${token}`
-        }
-    })
-        .then(result => {
-            const packages = result.repository.packages.nodes;
-            // Например, фильтруем только npm-пакеты
-            //const npmPackages = packages.filter(pkg => pkg.packageType.toLowerCase() === 'docker');
-            console.log("Packages:", packages);
-        })
-        .catch(error => {
-            console.error("Error:", error);
+    try {
+        const result = await graphql(query, {
+            owner,
+            name: repo,
+            headers: {
+                authorization: `token ${token}`
+            }
         });
 
+        const packages = result.repository.packages.nodes;
 
+        // Фильтруем пакеты, связанные с текущим репозиторием
+        const associatedPackages = packages.filter(pkg => pkg.name.includes(repo));
+
+        console.log("Associated Packages:", associatedPackages);
+    } catch (error) {
+        console.error("Error:", error);
+    }
 }
-
 
 run();
