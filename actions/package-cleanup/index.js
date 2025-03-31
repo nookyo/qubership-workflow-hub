84d3911e526457dc7a691464ca56e7dda45b70a8
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 14:57:45 +0400

    fix: Refactor package retrieval logic and enhance logging for user packages

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -7,35 +7,50 @@ async function run() {
 
     try {
 
-        const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-        const response = await octokit.request('GET /users/{username}', {
-            username: owner
-        });
 
-        const userType = response.data.type; // 'User' или 'Organization'
-        console.log(`Type: ${userType}`);
+        const octokit = new Octokit({
+            auth: env.GITHUB_TOKEN,
+          })
 
-        if (userType === 'User') {
-            console.log(`${owner} it is User.`);
-        } else if (userType === 'Organization') {
-            console.log(`${owner} it is Org.`);
-        }
-
-        const package = await octokit.request('GET /users/{username}/packages', {
+         const response = await octokit.request('GET /users/{username}/packages', {
             username: 'nookyo',
-            package_type: 'container',
-        });
-        console.log("Package version:", package.data);
-
-        const package_version = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
-            username: 'nookyo',
-            package_type: 'container',
-            package_name: 'qubership-dbaas',
             headers: {
-                'X-GitHub-Api-Version': '2022-11-28'
+              'X-GitHub-Api-Version': '2022-11-28'
             }
-        });
-        console.log("Package version:", package_version.data);
+          })
+
+        console.log("Package:", response.data);
+
+        // const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+        // const response = await octokit.request('GET /users/{username}', {
+        //     username: owner
+        // });
+
+        // const userType = response.data.type; // 'User' или 'Organization'
+        // console.log(`Type: ${userType}`);
+
+        // if (userType === 'User') {
+        //     console.log(`${owner} it is User.`);
+        // } else if (userType === 'Organization') {
+        //     console.log(`${owner} it is Org.`);
+        // }
+
+        // const package = await octokit.request('GET /users/{username}/packages', {
+        //     username: 'nookyo',
+        //     package_type: 'container',
+        // });
+        // console.log("Package version:", package.data);
+
+        // const package_version = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
+        //     username: 'nookyo',
+        //     package_type: 'container',
+        //     package_name: 'qubership-dbaas',
+        //     headers: {
+        //         'X-GitHub-Api-Version': '2022-11-28'
+        //     }
+        // });
+        // console.log("Package version:", package_version.data);
+
     } catch (error) {
         console.error("error:", error.message);
         console.error("detail:", error.package_version?.data || error);
