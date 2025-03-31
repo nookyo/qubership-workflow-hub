608d37c3538bf10d2cfa70fda7e9a075519dfbfc
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 11:32:56 +0400

    feat: Add fast-content-type-parse module and update @octokit/graphql to version 8.2.1

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { graphql } = require('@octokit/graphql');
 
 
 
@@ -9,7 +10,44 @@ async function run() {
     console.log(`Assembly ru on: ${repository.owner}/${repository.repo}`);
     console.log(`event: ${github.context.eventName}`);
 
-    const {owner, repo} = process.env.GIHUB_REPOSITORY.split('/');
+    const token = process.env.GITHUB_TOKEN;
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+    const octokit = github.getOctokit(token);
+
+    const query = `
+    query($owner: String!, $name: String!) {
+      repository(owner: $owner, name: $name) {
+        packages(first: 100) {
+          nodes {
+            name
+            packageType
+            latestVersion {
+              version
+            }
+          }
+        }
+      }
+    }
+  `;
+
+    graphql(query, {
+        owner,
+        name: repo,
+        headers: {
+            authorization: `token ${token}`
+        }
+    })
+        .then(result => {
+            const packages = result.repository.packages.nodes;
+            // Например, фильтруем только npm-пакеты
+            const npmPackages = packages.filter(pkg => pkg.packageType.toLowerCase() === 'npm');
+            console.log("NPM пакеты:", npmPackages);
+        })
+        .catch(error => {
+            console.error("Ошибка при получении пакетов:", error);
+        });
+
+
 }
 
 
