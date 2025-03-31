a08504462d9a0344ed0948fb89b10b6ce180c96d
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 14:46:15 +0400

    fix: Enhance package retrieval to include user type check and log package version

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -2,23 +2,39 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-  const token = process.env.GITHUB_TOKEN;
-  const octokit = github.getOctokit(token);
+    const token = process.env.GITHUB_TOKEN;
+    const octokit = github.getOctokit(token);
 
-  try {
-    const response = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
-      username: 'nookyo',
-      package_type: 'container',
-      package_name: 'qubership-dbaas',
-      headers: {
-         'X-GitHub-Api-Version': '2022-11-28'
-      }
-    });
-    console.log("Все версии пакета:", response.data);
-  } catch (error) {
-    console.error("Ошибка:", error.message);
-    console.error("Детали:", error.response?.data || error);
-  }
+    try {
+
+        const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+        const response = await octokit.request('GET /users/{username}', {
+            username: owner
+        });
+
+        const userType = response.data.type; // 'User' или 'Organization'
+        console.log(`Type: ${userType}`);
+
+        if (userType === 'User') {
+            console.log(`${username} it is User.`);
+        } else if (userType === 'Organization') {
+            console.log(`${username} it is Org.`);
+        }
+
+
+        const package_version = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
+            username: 'nookyo',
+            package_type: 'container',
+            package_name: 'qubership-dbaas',
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
+        console.log("Package version:", package_version.data);
+    } catch (error) {
+        console.error("error:", error.message);
+        console.error("detail:", error.package_version?.data || error);
+    }
 }
 
 run();
