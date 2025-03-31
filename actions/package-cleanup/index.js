bbdc2bb7008ac5ff1ab314c9a6baa74cbbdf95fa
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 15:30:34 +0400

    fix: Update package retrieval to use Octokit for listing user packages in package cleanup action

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -7,31 +7,11 @@ async function run() {
   const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
   
   try {
-    const query = `
-      query($owner: String!, $repo: String!) {
-        repository(owner: $owner, name: $repo) {
-          packages(first: 100) {
-            nodes {
-              name
-              packageType
-              latestVersion {
-                version
-              }
-            }
-          }
-        }
-      }
-    `;
-  
-    const result = await graphql(query, {
-      owner,
-      repo,
-      headers: {
-        authorization: `token ${token}`,
-        // Если потребуется — можно добавить заголовок предварительного просмотра:
-        accept: "application/vnd.github.package-deletes-preview+json"
-      }
-    });
+    const response = await octokit.rest.packages.listPackagesForUser({
+        username: 'nookyo',
+        package_type: 'docker' // или 'maven', 'rubygems', 'nuget', 'docker'
+      });
+      console.log(response.data);
   
     console.log("Repository packages:", result.repository.packages.nodes);
   } catch (error) {
