338ada796f634e5b4579e1a2b2700d1279590ca8
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 15:06:20 +0400

    fix: Remove commented-out code in package cleanup action

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -7,9 +7,6 @@ async function run() {
 
     try {
 
-
-
-
          const response = await octokit.request('GET /users/{username}/packages', {
             username: 'nookyo',
             package_type: 'container',
@@ -20,35 +17,6 @@ async function run() {
 
         console.log("Package:", response.data);
 
-        // const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
-        // const response = await octokit.request('GET /users/{username}', {
-        //     username: owner
-        // });
-
-        // const userType = response.data.type; // 'User' или 'Organization'
-        // console.log(`Type: ${userType}`);
-
-        // if (userType === 'User') {
-        //     console.log(`${owner} it is User.`);
-        // } else if (userType === 'Organization') {
-        //     console.log(`${owner} it is Org.`);
-        // }
-
-        // const package = await octokit.request('GET /users/{username}/packages', {
-        //     username: 'nookyo',
-        //     package_type: 'container',
-        // });
-        // console.log("Package version:", package.data);
-
-        // const package_version = await octokit.request('GET /users/{username}/packages/{package_type}/{package_name}/versions', {
-        //     username: 'nookyo',
-        //     package_type: 'container',
-        //     package_name: 'qubership-dbaas',
-        //     headers: {
-        //         'X-GitHub-Api-Version': '2022-11-28'
-        //     }
-        // });
-        // console.log("Package version:", package_version.data);
 
     } catch (error) {
         console.error("error:", error.message);
