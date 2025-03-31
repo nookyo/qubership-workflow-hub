8e1f52fa8891cb7e48c94f9c538be8d6dcc8cda0
Pavel Anikin <snwteam5@gmail.com>
2025-03-31 11:30:07 +0400

    fix: Correct typo in environment variable name for repository extraction

--- actions/package-cleanup/index.js
+++ actions/package-cleanup/index.js
@@ -8,6 +8,8 @@ async function run() {
     const repository = github.context.repo;
     console.log(`Assembly ru on: ${repository.owner}/${repository.repo}`);
     console.log(`event: ${github.context.eventName}`);
+
+    const {owner, repo} = process.env.GIHUB_REPOSITORY.split('/');
 }
 
 
