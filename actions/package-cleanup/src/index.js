62bdf2be6575d167f497dee0de2d5a1492695bff
Pavel Anikin <snwteam5@gmail.com>
2025-04-01 14:01:42 +0400

    add js

--- actions/package-cleanup/src/index.js
+++ actions/package-cleanup/src/index.js
@@ -3,6 +3,11 @@ const github = require("@actions/github");
 
 async function run() {
 
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+
+    console.log(`owner ${owner}`)
+    console.log(`repo ${repo}`)
+
 }
 
 run();
