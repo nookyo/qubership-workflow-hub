12574c38219553347fd980f009c2dd3da9fafe2e
Pavel Anikin <snwteam5@gmail.com>
2025-04-01 19:04:17 +0400

    fix: Remove version logging from package cleanup action

--- actions/package-cleanup/src/index.js
+++ actions/package-cleanup/src/index.js
@@ -35,7 +35,6 @@ async function run() {
     package.data.map(async (pkg) => {
 
         console.log(`Package: ${pkg.name}`);
-        console.log(`Version: ${JSON.stringify(version.data)}`);
 
         const version = await octokit.request('GET /users/{username}/packages/{package_name}/versions', {
             username: owner,
