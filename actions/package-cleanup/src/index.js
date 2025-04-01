3303df15c94151b13055fd800668bd53788e627f
Pavel Anikin <snwteam5@gmail.com>
2025-04-01 19:47:03 +0400

    fix: Update package cleanup thresholdDays to 7 for less frequent cleanups

--- actions/package-cleanup/src/index.js
+++ actions/package-cleanup/src/index.js
@@ -3,7 +3,7 @@ const github = require("@actions/github");
 
 async function run() {
 
-    const thresholdDays = 1;
+    const thresholdDays = 7;
     const excludedTags = ["main", "anotherTag"];
 
     const now = new Date();
