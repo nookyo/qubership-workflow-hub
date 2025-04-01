67d4921287fb9648e0c1587e2b3dc107462351d3
Pavel Anikin <snwteam5@gmail.com>
2025-04-01 14:05:34 +0400

    feat: Enhance package cleanup action to retrieve and log Docker conflict data

--- actions/package-cleanup/src/index.js
+++ actions/package-cleanup/src/index.js
@@ -5,8 +5,17 @@ async function run() {
 
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
-    console.log(`owner ${owner}`)
-    console.log(`repo ${repo}`)
+    const token = process.env.GITHUB_TOKEN;
+    const octokit = github.getOctokit(token);
+
+    let respond =  await octokit.request('GET /orgs/{org}/docker/conflicts', {
+        package_type: 'container',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+
+    console.log(`Respond data: ${respond.data}`);
 
 }
 
