8cf2bb7cf01ba1400dbbc9818fa40214a63fae04
Pavel Anikin <snwteam5@gmail.com>
2025-04-01 14:46:02 +0400

    fix: Update authentication method in package cleanup action to use PACKAGE_TOKEN

--- actions/package-cleanup/src/index.js
+++ actions/package-cleanup/src/index.js
@@ -6,7 +6,8 @@ async function run() {
     const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
     const token = process.env.GITHUB_TOKEN;
-    const octokit = github.getOctokit(token);
+    const package_token = process.env.PACKAGE_TOKEN;
+    const octokit = github.getOctokit(package_token);
 
     let respond = await octokit.request('GET /user/packages', {
         package_type: 'maven',
