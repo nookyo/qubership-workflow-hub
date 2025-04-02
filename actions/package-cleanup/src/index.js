1df7783e7dd74a12640406c51e74263569df5cdc
Pavel Anikin <snwteam5@gmail.com>
2025-04-02 16:54:59 +0400

    fix: Remove trailing whitespace in index.js

--- actions/package-cleanup/src/index.js
+++ actions/package-cleanup/src/index.js
@@ -1,6 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
-const OctokitWrapper = require("./wrapper");
+const OctokitWrapper = require("./wrapper"); 
 
 async function run() {
 
