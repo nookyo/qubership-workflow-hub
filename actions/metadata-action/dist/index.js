b3a54b90a6e632cdae49383baf32665140e3aab9
Pavel Anikin <snwteam5@gmail.com>
2025-05-14 17:03:38 +0400

    fix: simplify default template and tag retrieval logic for better readability

--- actions/metadata-action/dist/index.js
+++ actions/metadata-action/dist/index.js
@@ -42847,23 +42847,23 @@ async function run() {
   const loader = new ConfigLoader()
   const config = loader.load(configurationPath, debug);
 
-  // const defaultTemplate = core.getInput('default-template') || config["default-template"] || `{{ref-name}}-{{timestamp}}-{{runNumber}}`;
-  // const defaultTag = core.getInput('default-tag') || config["default-tag"] || "latest";
+  const defaultTemplate = core.getInput('default-template') || config?.["default-template"] || `{{ref-name}}-{{timestamp}}-{{runNumber}}`;
+  const defaultTag = core.getInput('default-tag') || config?.["default-tag"] || "latest";
 
-const inputTemplate = core.getInput('default-template');
-const inputTag      = core.getInput('default-tag');
+  // const inputTemplate = core.getInput('default-template');
+  // const inputTag = core.getInput('default-tag');
 
-const defaultTemplate = inputTemplate !== ""
-  ? inputTemplate
-  : (config["default-template"] && config["default-template"] !== ""
-     ? config["default-template"]
-     : `{{ref-name}}-{{timestamp}}-{{runNumber}}`);
+  // const defaultTemplate = inputTemplate !== ""
+  //   ? inputTemplate
+  //   : (config["default-template"] && config["default-template"] !== ""
+  //     ? config["default-template"]
+  //     : `{{ref-name}}-{{timestamp}}-{{runNumber}}`);
 
-const defaultTag = inputTag !== ""
-  ? inputTag
-  : (config["default-tag"] && config["default-tag"] !== ""
-     ? config["default-tag"]
-     : "latest");
+  // const defaultTag = inputTag !== ""
+  //   ? inputTag
+  //   : (config["default-tag"] && config["default-tag"] !== ""
+  //     ? config["default-tag"]
+  //     : "latest");
 
 
 
