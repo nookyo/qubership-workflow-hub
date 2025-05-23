24fe602209a330d36ba1976a2e2ae805603ef5a4
Pavel Anikin <snwteam5@gmail.com>
2025-05-23 00:54:23 -0700

    refactor: uncomment and implement filtering logic for container type packages in run function

--- actions/container-package-cleanup/src/index.js
+++ actions/container-package-cleanup/src/index.js
@@ -68,46 +68,46 @@ async function run() {
     })
   );
 
-  // filter for container type
-  // let filteredPackagesWithVersionsForDelete = packagesWithVersions.map(({ package: pkg, versions }) => {
+  //filter for container type
+  let filteredPackagesWithVersionsForDelete = packagesWithVersions.map(({ package: pkg, versions }) => {
 
-  //   const verisonWithOutExclude = versions.filter((version) => {
-  //     const createdAt = new Date(version.created_at);
-  //     const isOldEnough = createdAt <= thresholdDate;
+    const verisonWithOutExclude = versions.filter((version) => {
+      const createdAt = new Date(version.created_at);
+      const isOldEnough = createdAt <= thresholdDate;
 
-  //     if (!isOldEnough) return false;
-  //     if (!version.metadata || !version.metadata.container || !Array.isArray(version.metadata.container.tags)) return false;
-  //     const tags = version.metadata.container.tags;
+      if (!isOldEnough) return false;
+      if (!version.metadata || !version.metadata.container || !Array.isArray(version.metadata.container.tags)) return false;
+      const tags = version.metadata.container.tags;
 
-  //     if (excludedTags.length > 0 && tags.some(tag => excludedTags.some(pattern => wildcardMatch(tag, pattern)))) {
-  //       return false;
-  //     }
-  //     return true;
-  //   });
+      if (excludedTags.length > 0 && tags.some(tag => excludedTags.some(pattern => wildcardMatch(tag, pattern)))) {
+        return false;
+      }
+      return true;
+    });
 
-  //   const versionsToDelete = includedTags.length > 0 ? verisonWithOutExclude.filter((version) => {
-  //     if (!version.metadata || !version.metadata.container || !Array.isArray(version.metadata.container.tags)) return false;
-  //     const tags = version.metadata.container.tags;
-  //     return tags.some(tag => includedTags.some(pattern => wildcardMatch(tag, pattern)));
-  //   }) : verisonWithOutExclude;
+    const versionsToDelete = includedTags.length > 0 ? verisonWithOutExclude.filter((version) => {
+      if (!version.metadata || !version.metadata.container || !Array.isArray(version.metadata.container.tags)) return false;
+      const tags = version.metadata.container.tags;
+      return tags.some(tag => includedTags.some(pattern => wildcardMatch(tag, pattern)));
+    }) : verisonWithOutExclude;
 
-  //   const customPackage = {
-  //     id: pkg.id,
-  //     name: pkg.name,
-  //     type: pkg.package_type
-  //   };
+    const customPackage = {
+      id: pkg.id,
+      name: pkg.name,
+      type: pkg.package_type
+    };
 
-  //   return { package: customPackage, versions: versionsToDelete };
+    return { package: customPackage, versions: versionsToDelete };
 
-  // }).filter(item => item !== null && item.versions.length > 0);
+  }).filter(item => item !== null && item.versions.length > 0);
 
-  // if (filteredPackagesWithVersionsForDelete.length === 0) {
-  //   core.info("❗️ No versions to delete.");
-  //   return;
-  // }
+  if (filteredPackagesWithVersionsForDelete.length === 0) {
+    core.info("❗️ No versions to delete.");
+    return;
+  }
 
-  const strategy = new ContainerStrategy();
-  let filteredPackagesWithVersionsForDelete = await strategy.execute(packagesWithVersions, excludedTags, includedTags, thresholdDate);
+  // const strategy = new ContainerStrategy();
+  // let filteredPackagesWithVersionsForDelete = await strategy.execute(packagesWithVersions, excludedTags, includedTags, thresholdDate);
 
   if (isDebug) {
     core.info(`💡 Packages name: ${JSON.stringify(packagesNames, null, 2)}`);
