8c0a7d88ce7c774fb6d104f1b7f4bd31283afdf6
Pavel Anikin <snwteam5@gmail.com>
2025-05-23 00:37:21 -0700

    refactor: update package name and version in package-lock.json; modify MavenStrategy class structure
    - Changed package name from "package-cleanup" to "container-package-cleanup" and updated version to 1.2.0 in package-lock.json.
    - Renamed strategy class from "ContainerStrategy" to "MavenStrategy" for clarity.
    - Removed commented-out code in MavenStrategy's execute method and added a console log for package names.

--- actions/container-package-cleanup/src/strategy/maven.js
+++ actions/container-package-cleanup/src/strategy/maven.js
@@ -1,43 +1,16 @@
 class MavenStrategy {
     constructor() {
-        this.name = 'ContainerStrategy';
+        this.name = 'MavenStrategy';
     }
 
     async execute(packagesWithVersions, excludedTags, includedTags, thresholdDate) {
         let filteredPackagesWithVersionsForDelete = packagesWithVersions;
-        // let filteredPackagesWithVersionsForDelete = packagesWithVersions.map(({ package: pkg, versions }) => {
+        
+     packagesWithVersions.array.forEach(element => {
+        console.log(element.name);
+     });
 
-        //     const verisonWithOutExclude = versions.filter((version) => {
-        //         const createdAt = new Date(version.created_at);
-        //         const isOldEnough = createdAt <= thresholdDate;
-
-        //         if (!isOldEnough) return false;
-        //         if (!version.metadata || !version.metadata.container || !Array.isArray(version.metadata.container.tags)) return false;
-        //         const tags = version.metadata.container.tags;
-
-        //         if (excludedTags.length > 0 && tags.some(tag => excludedTags.some(pattern => wildcardMatch(tag, pattern)))) {
-        //             return false;
-        //         }
-        //         return true;
-        //     });
-
-        //     const versionsToDelete = includedTags.length > 0 ? verisonWithOutExclude.filter((version) => {
-        //         if (!version.metadata || !version.metadata.container || !Array.isArray(version.metadata.container.tags)) return false;
-        //         const tags = version.metadata.container.tags;
-        //         return tags.some(tag => includedTags.some(pattern => wildcardMatch(tag, pattern)));
-        //     }) : verisonWithOutExclude;
-
-        //     const customPackage = {
-        //         id: pkg.id,
-        //         name: pkg.name,
-        //         type: pkg.package_type
-        //     };
-
-        //     return { package: customPackage, versions: versionsToDelete };
-
-        // }).filter(item => item !== null && item.versions.length > 0);
-
-        return filteredPackagesWithVersionsForDelete;
+     return filteredPackagesWithVersionsForDelete;
     }
 }
 
