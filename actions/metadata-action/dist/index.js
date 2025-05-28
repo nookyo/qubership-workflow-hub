b707825dc8ad4c8a401100effd1f4f61aebe0692
Pavel Anikin <snwteam5@gmail.com>
2025-05-28 12:12:01 +0500

    fix: improve template validation in findTemplate function

--- actions/metadata-action/dist/index.js
+++ actions/metadata-action/dist/index.js
@@ -42797,8 +42797,8 @@ function matchesPattern(refName, pattern) {
 }
 
 function findTemplate(refName, templates) {
-  if (templates === undefined || templates === null || templates.length === 0) return null;
-  
+  if (!Array.isArray(templates) || templates.length === 0) return null;
+
   for (let item of templates) {
     let pattern = Object.keys(item)[0];
     if (matchesPattern(refName, pattern)) {
