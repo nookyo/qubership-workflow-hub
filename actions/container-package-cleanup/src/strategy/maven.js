770b22017155391f606bb897d6d8d3cb8652730a
Pavel Anikin <snwteam5@gmail.com>
2025-06-19 15:00:51 +0400

    fix: remove unnecessary debug grouping for package version checks in MavenStrategy

--- actions/container-package-cleanup/src/strategy/maven.js
+++ actions/container-package-cleanup/src/strategy/maven.js
@@ -18,9 +18,7 @@ class MavenStrategy {
                 const createdAt = new Date(version.created_at);
                 const isOldEnough = createdAt <= thresholdDate;
 
-                debug && core.info(`::group:: Package version check.`);
                 debug && core.info(`Checking package: ${pkg.name} version: ${version.name}, created at: ${createdAt}, Threshold date: ${thresholdDate}, Is old enough: ${isOldEnough}`);
-                debug && core.info(`::endgroup::`);
 
                 if (!isOldEnough) return false;
 
