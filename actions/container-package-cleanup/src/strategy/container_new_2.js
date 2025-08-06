ee1d021c188a6101f7c224f92d12c4bf278298b3
Pavel Anikin <snwteam5@gmail.com>
2025-08-06 19:12:54 +0400

    fix: update execute method to return parsed packages instead of packagesWithVersions

--- actions/container-package-cleanup/src/strategy/container_new_2.js
+++ actions/container-package-cleanup/src/strategy/container_new_2.js
@@ -116,7 +116,7 @@ class ContainerStrategy extends AbstractPackageStrategy {
 
         // let  t = deletePackageVersion(dangling);
 
-        const result = packagesWithVersions;
+        const result = packages;
 
 
         return result;
