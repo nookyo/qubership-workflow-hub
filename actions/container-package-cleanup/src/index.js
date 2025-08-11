fa4243cd8a41afd86fe0c9aa913b3952b9620f4c
Pavel Anikin <snwteam5@gmail.com>
2025-08-11 17:13:10 +0400

    fix: remove deprecated ContainerStrategy implementation and related code

--- actions/container-package-cleanup/src/index.js
+++ actions/container-package-cleanup/src/index.js
@@ -131,28 +131,8 @@ async function run() {
     return;
   }
 
-  // for (const { package: pkg, versions } of filteredPackagesWithVersionsForDelete) {
-  //   for (const version of versions) {
-  //     try {
-  //       let detail = pkg.type === 'maven' ? version.name : (version.metadata?.container?.tags ?? []).join(', ');
-  //       core.info(`Package: ${pkg.name} (${pkg.type}) — deleting version: ${version.id} (${detail})`);
-  //       await wrapper.deletePackageVersion(owner, pkg.type, pkg.name, version.id, isOrganization);
-
-  //     } catch (error) {
-  //       // Handle specific error for high download count
-  //       if (error.message.includes("Publicly visible package versions with more than 5000 downloads cannot be deleted")) {
-  //         core.warning(`Skipping version: ${version.id} (${version.metadata?.container?.tags?.join(', ')}) due to high download count.`);
-  //       } else {
-  //         core.error(`Failed to delete version: ${version.id} (${version.metadata?.container?.tags?.join(', ')}) — ${error.message}`);
-  //       }
-  //     }
-  //   }
-  // }
-
   try {
     if (!dryRun && filteredPackagesWithVersionsForDelete.length > 0) {
-      // let detail = pkg.type === 'maven' ? version.name : (version.metadata?.container?.tags ?? []).join(', ');
-      // core.info(`Package: ${pkg.name} (${pkg.type}) — deleting version: ${version.id} (${detail})`);
       await deletePackageVersion(filteredPackagesWithVersionsForDelete, { wrapper, owner, isOrganization });
     }
 
